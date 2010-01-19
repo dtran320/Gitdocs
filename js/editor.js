bc4a8c425b5c344083a906b64248dd4a000a00dc
Mark Linsey <mlinsey@dn0a201a0e.sunet>
2010-01-19 15:12:01 -0800

    Added editor.html

---
+++ js/editor.js
@@ -0,0 +1,14 @@
+function DisplayOthers() {
+	 $("#myversionspanel").hide();
+	 $("#otherversionspanel").show();
+	 $("#myversions_selected").hide();
+         $("#othersversions_selected").show();
+
+}
+
+function DisplayMine() {
+ 	$("#myversionspanel").show();
+        $("#otherversionspanel").hide();
+	 $("#myversions_selected").show();
+         $("#othersversions_selected").hide();
+}
