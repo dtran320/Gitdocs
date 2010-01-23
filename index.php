a3a6c70fd7fe923c9a1596f55dfd41d7389aca48
marcia <malee@ubuntu.ubuntu-domain>
2010-01-23 15:54:39 -0800

    using smarty template engine

--- index.php
+++ index.php
@@ -1,75 +1,20 @@
-<? include('header.php'); ?>
+<?
+require('smarty_lib/Smarty.class.php');
+$smarty = new Smarty();
+$smarty->template_dir = 'smarty/templates';
+$smarty->config_dir = 'smarty/configs';
+$smarty->compile_dir = 'smarty/templates_c';
+$smarty->cache_dir = 'smarty/cache';
 
- <div class="container center_container">
+$smarty->assign('name', 'Ned');
 
-<div class="left_side45">
-<div class="box">
-	<div class="box_title">Search Gitdocs</div>
-	<div class="box_content">
-	<form id="form_search" class="big_form" action="search.php" method="get">
-	<input type="text" name="query" value="Find a document" />
-	<input type="submit" value="Search" />
-	</form>
-	</div>
-</div>
+$my_recent_docs = array("doc1", "doc2", "doc3");
+$smarty->assign('my_recent_docs', $my_recent_docs);
 
-	<div class="box">
-			<div class="box_title">Create</div>
-			<div class="box_content">
-				<div id="doc_link_left">
-				<a href="index.php">New Document</a>
-				</div>
-				<div id="doc_link_right">
-				<a href="import.php">Upload document</a>
-				</div>
-				<div class="clearfix"></div>
-			</div>
-		</div>
+$smarty->assign('pop_tops', array("databases networks compilers os", "OOP closure languages", "datavis hci graphics siggraph", "anonymity identity audience cs294h"));
 
+$smarty->assign('pop_docs', array("<a href='http://nytimes.com'>asdfasdfasfd</a>", "<a href='http://nytimes.com'>jkljkljkljlk</a>", "<a href='viewer.php'>Twilight!!</a>", "<a href='http://nytimes.com'>iopiopiop</a>"));
 
-<div class="box">
-	 <div class="box_title">My Recent Documents</div>
-        <div class="box_content">
-			<table>
-			<tr><td>PhD Personal Statement</td></tr>
-			<tr><td>CS147 Notes</td></tr>
-			<tr><td>History Paper</td></tr>
-			</table>
-			<div style="padding-top:10px;">
-			<a href="index.php">See All My Documents</a>
-			</div>
-			
-		</div><!-- end box_content -->
-	</div><!-- end box -->
-	</div><!-- end container col-->
-<div class="right_side45">
+$smarty->display('index.tpl');
+?>
 
-<div class="box">
-	 <div class="box_title">Popular topics</div>
-       <div class="box_content">
-			<div style="padding-bottom:20px;">
-			<table>
-			<tr><td><span style="font-size:30px">lorem ipsum dolor </span></td></tr>
-			<tr><td><span style="font-size:25px">consectetur adipisicing</span></td></tr>
-			<tr><td><span style="font-size:20px">ed do eiusmod tempor </span></td></tr>
-			<tr><td><span style="font-size:15px">e et dolore magna aliqua occaecat</span></td></tr>
-			</table>
-			</div>
-		</div>
-</div>
-
-<div class="box">
-	 <div class="box_title">Popular documents</div>
-       <div class="box_content">
-			<table>
-			<tr><td>How to Get Rich</td></tr>
-			<tr><td><a href="viewer.php">Twilight Fanfic - Edward and Jacob!!</a></td></tr>
-			<tr><td>Quantum Field Theory for Idiots</td></tr>
-			</table>
-		</div>
-</div>
-</div>
-
-</div><!-- end container -->
-
-<? include('footer.php'); ?>
