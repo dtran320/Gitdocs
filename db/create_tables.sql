8708e300925fd6da1680c7521d1cdfe545a5c32f
David Tran <davetran@stanford.edu>
2010-01-24 17:29:48 -0800

    Modified create_tables.sql to enforce FK constraints and to be used generically with any DB.

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -1,5 +1,34 @@
-use gitdocs;
-CREATE TABLE IF NOT EXISTS Users(u_id VARCHAR(40) primary key, pwd_hash varchar(40) NOT NULL, display_name VARCHAR(40), icon_ptr VARCHAR(40));
-CREATE TABLE IF NOT EXISTS Documents(doc_id INTEGER primary key, name VARCHAR(40));
-CREATE TABLE IF NOT EXISTS Versions(doc_ID Integer references Documents(doc_id), u_id VARCHAR(40) references Users(u_id), repo_ptr VARCHAR(40));
-quit 
+--Creates the Users, Documents, and Versions table
+--use Gitdocs
+
+CREATE TABLE IF NOT EXISTS `Users`(
+	`u_id` INTEGER AUTO_INCREMENT,
+	PRIMARY KEY(`u_id`),
+	
+	`username` VARCHAR(40) UNIQUE,
+	
+	`pwd_hash` varchar(40) NOT NULL, 
+	`display_name` VARCHAR(40), 
+	`icon_ptr` VARCHAR(40)
+) ENGINE=INNODB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `Documents`(
+	`doc_id` INTEGER AUTO_INCREMENT,
+	PRIMARY KEY(`doc_id`),
+	
+	`name` VARCHAR(40)
+)ENGINE=INNODB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `Versions`(
+	`v_id` INTEGER AUTO_INCREMENT,
+	PRIMARY KEY(`v_id`),
+	
+	`doc_fk` INTEGER,
+	FOREIGN KEY(`doc_fk`) REFERENCES Documents(`doc_id`),
+	
+	`u_fk` INTEGER,
+	FOREIGN KEY(`u_fk`) REFERENCES Users(`u_id`),
+	
+	`repo_ptr` VARCHAR(40)
+)ENGINE=INNODB DEFAULT CHARSET=latin1;
+
