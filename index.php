6be6770bce387da5464d86ac01d56ddc135ded14
David Tran <davetran@stanford.edu>
2010-01-28 11:37:35 -0800

    some work on login

--- index.php
+++ index.php
@@ -4,14 +4,14 @@ session_start();
 require('classes/user.php');
 require('init_smarty.php');
 
-$my_recent_docs = array("doc1", "doc2", "doc3");
-$smarty->assign('my_recent_docs', $my_recent_docs);
-
 $smarty->assign('pop_tops', array("databases networks compilers os", "OOP closure languages", "datavis hci graphics siggraph", "anonymity identity audience cs294h"));
 
 $smarty->assign('pop_docs', array("<a href='http://nytimes.com'>asdfasdfasfd</a>", "<a href='http://nytimes.com'>jkljkljkljlk</a>", "<a href='viewer.php'>Twilight!!</a>", "<a href='http://nytimes.com'>iopiopiop</a>"));
 
 if($user = User::getLoggedInUser()) {
+	$my_recent_docs = array("doc1", "doc2", "doc3");
+	$smarty->assign('my_recent_docs', $my_recent_docs);
+	
 	$logged_in_user = array(
 							"iconPtr" => "images/mlinsey.jpg",
 							"displayName" => "Mark Linsey"
@@ -22,6 +22,8 @@ if($user = User::getLoggedInUser()) {
 	$smarty->display('index.tpl');
 }
 else {
+	$smarty->assign('headline', "Tell your version of the story.");
+	$smarty->assign('description', "Gitdocs");
 	$smarty->display('signup.tpl');
 }
 ?>
