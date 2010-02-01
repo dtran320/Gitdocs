9d18aaa79ded7b8894717e44ce81c9fd2c3ccd2d
Mark Linsey <mlinsey@mlinsey-desktop.(none)>
2010-02-01 10:28:49 -0800

    old (blank) version of testVersion, newer version doesn't work yet

---
+++ tests/testVersion.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once(dirname(__FILE__) . "/../config.php");
+
+require_once(dirname(__FILE__) . "/../classes/Version.php");
+
+function testCreateVersion() {
+	echo "Creating ";
+/*	$document = Document::createNewDocument("dtran320", "testdoc");
+		if($document)
+			echo "Created new document testdoc <br>";
+		else {
+			echo "Failed.<br/>";
+			return false;
+		}
+	
+	echo "Testing that document name is correct...";
+	echo ($document->name() == "testdoc")? "Yes!" : "No!";
+	
+	echo "<br/>";*/
+
+}
+
+?>
