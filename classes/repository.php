dd13125b07cbafa7ce7469eca16bd09ee38d16d6
Mark Linsey <mlinsey@dn0a211a62.sunet>
2010-02-02 15:13:12 -0800

    added repository class

---
+++ classes/repository.php
@@ -0,0 +1,58 @@
+<?php
+
+/* ----------------------------------------------------------------------------
+ * Version class
+ * 
+ * Gitdocs Jan 2010
+ * ---------------------------------------------------------------------------- */
+
+class Repository {
+
+	private $location;
+	private $currentBranch;
+	public function __construct($location) {
+		$this->location = $location;
+	}	
+	
+	public static function CreateNewRepository($docId, $userId,$versionToClone = 0) {
+		global $DOCUMENTS_PATH;
+		$location = "$DOCUMENTS_PATH//$docId//$userId";
+		if(!mkdir("$location", 0600)) return false;				  if(!versionToClone) {
+			$otherRepoLocation = $versionToClone->getRepoLocation();
+			$command = "$location//git clone $otherRepoLocation";
+			//TODO: Escape this? necessary?
+			exec($command);
+		} else {
+			//TODO: create starter document file here
+			if(!mkdir("$location//.git", 0600) return false);
+			$command = "$location//.git//git --bare init";
+			exec($command);
+		}
+		return new Repository($location);
+	}
+
+	public function GetLocation() {
+		return $location;
+	}
+	
+	public function commit() {
+		$command = "$location//git commit -a";
+		exec($command);
+		//TODO: label unsurfanced commits differently, bypass commit message
+	}
+	
+	public function getFile() {
+		$fh = fopen("$location//document.html",'w+');
+		return $fh;
+	}
+	
+	public function diff($otherVersion) {
+	}
+	
+	public function merge($otherVersion) {
+
+	}
+
+}
+
+?>
--- classes/version.php
+++ classes/version.php
@@ -5,6 +5,9 @@
  * 
  * Gitdocs Jan 2010
  * ---------------------------------------------------------------------------- */
+require_once(dirname(__FILE__) . "/../config.php");
+require_once(dirname(__FILE__) . "/../db/db.php");
+require_once(dirname(__FILE__) . "/repository.php");
 
 class Version {
 	
@@ -14,16 +17,29 @@ class Version {
 	private $lastSavedTime;
 	
 	private $commitId; //sha-1 hash
-
+	private $repo;
 	private $fileHandler;
 	
-	public function __construct($docId, $userId, $description = 0) {
+	public function __construct($docId, $userId, $repo = 0,$description = 0) {
 		$this->docId = $docId;	
 		$this->userId = $userId;
 		$this->description = $description;
+		if($repo)
+			$this->repo = $repo;
+		else 
+			$this->repo = new Repository($docId, $userId);
 	}
-	public static function CreateNewVersion($creator, $docId, $versionToClone, $description = 0) {
-	
+	public static function CreateNewVersion($creator, $docId, $versionToClone = 0, $description = 0) {
+		$repo = CreateNewRepository($docId, $userId, $versionToClone);
+		if(!$repo) return false;
+		$db = new DB();
+		$params = array("docId" => $docId, "userId" => $userId, "repoPtr" => $repo->getLocation());
+		mysqlEscapeArray($params);
+		$newVersionQuery = " $createUserQuery = "INSERT INTO Versions(doc_fk, u_fk, repo_ptr) " .
+                        "VALUES('{$params["docId"]}', '{$params["userId"]}', '{$arr["repoPtr"]}')";";
+
+		$db->execQuery($newVersionQuery);
+		return new Version($docId, $creator, $repo,$description);	
 	}
 
 	//returns array of Versions
@@ -33,19 +49,37 @@ class Version {
 	
 	//just saves, update lastSavedTime
 	public function save() {
-		
+		fclose($fileHandler);
+		//TODO: flesh out, merge with ckeditor	
 	}
 	
 	//saves, does git commit, returns new Version object
 	public function commit() {
-		
+		save();	
+		$repo->commit();
+		return this;
+	}
+
+	public function openVersionFile() {
+		$fileHandler = $repo->getFile();
+		return $fileHandler;
+	}	
+	
+	public function diff($otherVersion) {
+		return $repo->diff($otherVersion);
 	}
 	
-	/* Private instance methods */
-	private function openVersionFileFromDisk() {
+	public function merge($otherVersion, $diffs) {
+		//TODO:parse diffs, open other version, undo changes
 		
+		$repo->merge($otherVersion);
+		commit();
+		return true;
 	}
-	
+
+	public function getRepoLocation(){
+		return $repo->getLocation();
+	}
 }
 
 ?>
