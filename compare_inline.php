834671d4187961aa2dfa0a7780ad0debb627a20e
marcia <malee@ubuntu.ubuntu-domain>
2010-02-02 03:14:14 -0800

    sry didn't add new files

---
+++ compare_inline.php
@@ -0,0 +1,53 @@
+<?
+session_start();
+require('init_smarty.php');
+require('lib/simplediff.php');
+include_once "lib/Diff/Diff.php";
+include_once "lib/Diff/Diff/Renderer.php";
+include_once "lib/Diff/Diff/Renderer/inline.php";
+
+// temp..
+$smarty->assign('d_name', 'CS294 Class Notes');
+$smarty->assign('v_name', 'winter 2010');
+$smarty->assign('u_name', 'mlinsey');
+$smarty->assign('other_u_name', 'mlee');
+$smarty->assign('other_v_name', 'winter 2010');
+
+// we should flesh out all the different phrases instead of doing this:
+$smarty->assign(history, array(
+	array("left", "images/mlinsey.jpg","you are now editing <span class='v_name'>winter 2010</span>, which you saved 5m ago"),
+	array("right", "images/dtran.jpg","you started from dtran's <span class='v_name'>winter 2010</span> 5m ago"),
+
+	));
+$smarty->assign(others, array(
+	array('images/mlee.jpg', '<a class="v_name">winter 2010</a><br/>by mlee 8h ago'),
+	array('images/dtran.jpg', '<a class="v_name">winter 2010</a><br />by dtran 1d ago'),
+	array('images/bella8.jpg', '<a class="v_name">fall 2008</a><br />by bella8 2y ago'),
+	));
+
+$patch = file_get_contents('tests/example.patch');
+$diff = new Text_Diff('string', array($patch));
+$renderer = new Text_Diff_Renderer_inline();
+$out = $renderer->render($diff);
+
+$out = '<span style="font-weight: bold;" class="likedislike"><span class="like" onclick="likeAll_inline();">L</span> | <span class="dislike" onclick="dislikeAll_inline();">D</span> |<span class="undo" onclick="undoAll_inline();">U</span></span>' . $out;
+
+$out = str_replace("<ins>", "<div class='inline_change'><ins>", $out);
+$out = str_replace("</ins>", "</ins></div><div class='inline_ld'></div>", $out);
+$out = str_replace("<del>", "<div class='inline_change'><del>", $out);
+$out = str_replace("</del>", "</del></div><div class='inline_ld'></div>", $out);
+$out = str_replace("</del></div><div class='inline_ld'>like | dislike</div><div class='inline_change'><ins>", "</del><ins>", $out);
+
+
+/*
+$out = str_replace("<ins>", "<br/><ins>", $out);
+$out = str_replace("</ins>", "</ins><br/>", $out);
+$out = str_replace("<del>", "<br/><del>", $out);
+$out = str_replace("</del>", "</del><br/>", $out);
+$out = str_replace("<br/><br/>", "", $out);
+*/
+
+$smarty->assign('diff', $out);
+
+$smarty->display('compare_inline.tpl');
+?>
---
+++ smarty/templates/compare_inline.tpl
@@ -0,0 +1,58 @@
+{include file="header.tpl"}
+
+<div class="left_main">
+	<div class="box" style="width: 600px;">
+	<div class="box_title">Comparing -- {$d_name} <a href="compare.php" style="color: red;">==AB TESTING CLICK HERE==</a></div>
+	<div class="box_content" style="font-size: 13px;">
+	{$diff}
+	</div> <!--box content-->
+	</div>
+</div> <!-- left_main-->
+
+<div class="box right_side">
+  <div class="box_title">
+	<ul class = "tabs primary" id ="myversions_selected" style="display:none">
+		<li class = "active" id="myversions_tab"><span onclick="DisplayMine()"><a class="active">History</a></span></li>
+		<li><span onclick="DisplayOthers()"><a>Classmates</a></span></li>
+	</ul>
+	<ul class="tabs primary" id="othersversions_selected" style="display:block;">
+	 <li id="myversions_tab"><span onclick="DisplayMine()"><a>History</span></a></li>
+	 <li class="active"><span onclick="DisplayOthers()"><a class="active">Classmates</span></a></li>
+	</ul>
+	</div><!-- box_title-->
+  <div class="box_content" id="myversionspanel" style="display:none;">
+  	<table>
+			{section name=i loop=$history}	
+				<tr><td><div style="float: {$history[i][0]}; padding-left: 6px; padding-right:6px;"><img src="{$history[i][1]}" /> </div><div class="med_text align_{$history[i][0]}">{$history[i][2]}</div></td></tr>
+			{/section}
+		</table> 
+	</div>
+	
+	<div class="box_content" id="otherversionspanel" style="display:block;">
+		<table style="width:100%">
+			{section name=i loop=$others}	
+			<tr><td id="td_{$smarty.section.i.index}"
+					class="selectable {if $smarty.section.i.index == 0}selected{/if}"
+					onclick="change_selection({$smarty.section.i.index})">
+					<div style="float: left; padding-right:6px;"><img src="{$others[i][0]}" /> </div>
+					<div class="med_text"> 
+						<span style="float:left">{$others[i][1]}</span>
+						<a class="comparable">compare</a>
+					</div>
+			</td></tr>
+			{/section}
+		</table>
+	<p>these are awkward line breaks, but makes the like|dislike fit nicely one per line</p>	
+	</div>
+</div>
+	<script type="text/javascript">
+	{literal}
+	//<![CDATA[
+		$(document).ready(function(){
+			addLikeDislikeLinks();	
+		});
+	//]]>
+	{/literal}
+	</script>
+
+{include file="footer.tpl"}
