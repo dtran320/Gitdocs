3ea981d7da7c00bb4ced02c8597c02f154c13629
marcia <malee@ubuntu.ubuntu-domain>
2010-02-21 21:56:58 -0800

    on upload, automatically crops a square in case user doesn't

--- actions/crop.php
+++ actions/crop.php
@@ -3,7 +3,7 @@ require_once(dirname(__FILE__) . '/../lib/utils.php');
 require_once(dirname(__FILE__) . "/../config.php");
 
 $action = postVarClean('action');
-$u_id = postVarClean('u_id') != "" ? postVarClean('u_id') : getVarClean('u_id');
+$u_id = postVarClean('u_id');
 
 
 $big_filename = dirname(__FILE__) . '/../' . $AVATARS_PATH . $u_id . '_big.jpg';
@@ -11,7 +11,7 @@ $small_filename = dirname(__FILE) . '/../' . $AVATARS_PATH . $u_id . '_small.jpg
 
 //TODO: 
 // file size
-// file type (convert to jpg?)
+// file type (convert to jpg from png, etc)
 // use success/failure msgs 
 
 if ($action == 'upload') {
@@ -20,6 +20,7 @@ if ($action == 'upload') {
 		if (file_exists($small_filename)) {
 			unlink($small_filename);
 		}
+		crop_image(0, 0, 100, 100, $big_filename, $small_filename);
 		echo 'Pic upload success';
 	} else{
 	  echo "There was an error uploading the file, please try again!";
@@ -30,7 +31,10 @@ if ($action == 'upload') {
 	$y = postVarClean('y');
 	$w = postVarClean('w');
 	$h = postVarClean('h');
+	crop_image($x, $y, $w, $h, $big_filename, $small_filename);
+} 
 
+function crop_image($x, $y, $w, $h, $big_filename, $small_filename) {
 	$targ_w = $targ_h = 50;
 	$jpeg_quality = 90;
 
@@ -39,42 +43,15 @@ if ($action == 'upload') {
 
 	imagecopyresampled($dst_r, $img_r, 0, 0, $x, $y, $targ_w, $targ_h, $w, $h);
 
-	if(imagejpeg($dst_r, $small_filename, $jpeg_quality)) {
-		echo 'Success';
-	} else {
-		echo 'There was an error saving.';
-	}
+	$success = imagejpeg($dst_r, $small_filename, $jpeg_quality);
 
 	imagedestroy($img_r);
 	imagedestroy($dst_r);
-} else {
-
-/*
-	$type = getVarClean('type');
-	$a = getVarClean('a');
-	if ($type == 'cropped') {
-		$filename = $small_filename;
-	} else if ($a == 'a'){
-		$filename = $bigg;
+	if ($success) {
+		echo 'Success';
 	} else {
-		$filename = $big_filename;
+		echo 'Error';
 	}
-	if(file_exists($filename)) {
-		$img = imagecreatefromjpeg($filename);		
-	} else {
-		$img = imagecreatefromjpeg('../images/default.jpg');
-	}	
-	
-	
-	header('Content-type: image/jpeg');
-//	header('Expires: Thu, 01 Dec 1994 16:00:00 GMT');
-//	header('Cache-Control: no-cache, no-store');
-//	header('Last-Modified: '.gmdate('D, d M Y H:i:s', time()).' GMT'); 
-
-	imagejpeg($img);
-	imagedestroy($img);
-*/
 }
-
 
 ?>
