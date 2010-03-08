69213992d89bea97fdc87134ab6bae8a430dc147
David Tran <dtran@inglorious.Stanford.EDU>
2010-03-07 22:59:09 -0800

    website optimizer + other bug fixes

--- new_note.php
+++ new_note.php
@@ -4,7 +4,10 @@ require_once('config.php');
 session_start();
 require_once('classes/user.php');
 require_once('classes/document.php');
+require_once('classes/version.php');
+require_once('lib/twitter.php');
 require('init_smarty.php');
+
 
 if($user = User::getLoggedInUser()) {
 	$smarty->assign('logged_in_user', $user->getUserInfo());
@@ -23,11 +26,11 @@ else {
 
 	$recent_global_docs = Version::getRecentGlobalVersionsClean(5);
 	$smarty->assign('recent_global_docs', $recent_global_docs);
-	$smarty->assign('gitdocs_description', "<p>Why study for tests in isolation? Want to share notes but not sure how to do so effectively? 
-	Gitdocs allows you to upload your class notes and manage sections of notes from your classmates,
-	choosing only those contributions you feel will be helpful to your version of your notes.</p>");
+	$smarty->assign('gitdocs_description', "<p>Why study for tests in isolation? Want to share notes but not sure how to do so effectively? Gitdocs allows you to upload your class notes and manage sections of notes from your classmates, choosing only those contributions you feel will be helpful to your version of your notes.</p>");
 	$smarty->display('signup.tpl');
 }
+
+?>
 <!-- Google Website Optimizer Conversion Script -->
 <script type="text/javascript">
 if(typeof(_gat)!='object')document.write('<sc'+'ript src="http'+
@@ -39,5 +42,3 @@ var gwoTracker=_gat._getTracker("UA-9743398-2");
 gwoTracker._trackPageview("/2637776227/goal");
 }catch(err){}</script>
 <!-- End of Google Website Optimizer Conversion Script -->
-
-?>
