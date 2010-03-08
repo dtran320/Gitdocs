598b258fe8ce64570eafe44349e60631ce8d91a5
David Tran <dtran@inglorious.Stanford.EDU>
2010-03-07 22:56:02 -0800

    website optmizer

--- new_note.php
+++ new_note.php
@@ -18,6 +18,26 @@ if($user = User::getLoggedInUser()) {
 	
 }
 else {
-	header('Location: signup.php');
+	$twitter_updates = getGitdocsUpdates(3);
+	$smarty->assign('twitter_updates', $twitter_updates);
+
+	$recent_global_docs = Version::getRecentGlobalVersionsClean(5);
+	$smarty->assign('recent_global_docs', $recent_global_docs);
+	$smarty->assign('gitdocs_description', "<p>Why study for tests in isolation? Want to share notes but not sure how to do so effectively? 
+	Gitdocs allows you to upload your class notes and manage sections of notes from your classmates,
+	choosing only those contributions you feel will be helpful to your version of your notes.</p>");
+	$smarty->display('signup.tpl');
 }
+<!-- Google Website Optimizer Conversion Script -->
+<script type="text/javascript">
+if(typeof(_gat)!='object')document.write('<sc'+'ript src="http'+
+(document.location.protocol=='https:'?'s://ssl':'://www')+
+'.google-analytics.com/ga.js"></sc'+'ript>')</script>
+<script type="text/javascript">
+try {
+var gwoTracker=_gat._getTracker("UA-9743398-2");
+gwoTracker._trackPageview("/2637776227/goal");
+}catch(err){}</script>
+<!-- End of Google Website Optimizer Conversion Script -->
+
 ?>
