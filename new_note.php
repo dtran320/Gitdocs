3bd91c7d17c8e9c2ab121ba9b11a3a264f973102
David Tran <dtran@DN0a211bdd.SUNet>
2010-03-18 12:06:31 -0700

    fixed weird error

--- new_note.php
+++ new_note.php
@@ -21,14 +21,6 @@ if($user = User::getLoggedInUser()) {
 	
 }
 else {
-	$twitter_updates = getGitdocsUpdates(3);
-	$smarty->assign('twitter_updates', $twitter_updates);
-
-	$recent_global_docs = Version::getRecentGlobalVersionsClean(5);
-	$smarty->assign('recent_global_docs', $recent_global_docs);
-	$smarty->assign('gitdocs_description', "<p>Why study for tests in isolation? Want to share notes but not sure how to do so effectively? Gitdocs allows you to upload your class notes and manage sections of notes from your classmates, choosing only those contributions you feel will be helpful to your version of your notes.</p>");
-}
-else {
 	$_SESSION['return_page'] = 'new_note.php';
 	header('Location: signup.php?status=login');
 }
